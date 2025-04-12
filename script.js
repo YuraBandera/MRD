6f5457c5c761059f99c80f48b3ebb8d7145b38a7
YuraBandera <romansheretko2@gmail.com>
2025-04-12 13:48:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// Перемикач теми
-const themeToggle = document.getElementById('theme-toggle');
-themeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-});
-
-// Анімація для вкладок
-document.querySelectorAll('.animated-link').forEach(link => {
-  link.addEventListener('mouseover', () => {
-    link.classList.add('animated');
-  });
-  link.addEventListener('mouseout', () => {
-    link.classList.remove('animated');
-  });
+// Додаємо функціональність для перемикача теми
+document.getElementById("theme-toggle").addEventListener("change", function () {
+    document.body.classList.toggle("dark-theme", this.checked);
 });
